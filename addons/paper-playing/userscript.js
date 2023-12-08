00e60a9db9f88c2dc8e2b56a13739b3e4d09e093
Jazza <greeny-231@outlook.com>
2023-12-08 17:13:09 +0800

    idk

--- addons/paper-playing/userscript.js
+++ addons/paper-playing/userscript.js
@@ -1,13 +1,30 @@
 export default async function ({ addon, console }) {
   const paper = await addon.tab.traps.getPaper();
-  console.log(paper);
-  const fixGuideSizes = () => {
-    return new paper.Path.Circle({
-      center: new paper.Point(0, 0),
-      radius: 5.5 / paper.view.zoom,
-      fillColor: "black",
-      visible: true,
-    });
-  };
-  console.log(fixGuideSizes().position);
+  console.log(await addon.tab);
+
+  document.addEventListener("keydown", (e) => {
+    if (e.ctrlKey || e.metaKey) {
+      if (e.key === "e") {
+        // Prevent browser search opening
+        e.preventDefault();
+
+        const selected = paper.project.selectedItems;
+        let index = 0;
+        selected.forEach((item) => {
+          // Find the highest item selected
+          if (item.index > index) index = item.index;
+        });
+
+        let group = new paper.Group(selected);
+
+        group.position = new paper.Point(480, 360);
+        paper.tools[0].boundingBoxTool.setSelectionBounds(); // Draw the bounding box in the new position
+
+        group.remove(); // Remove the group so that it doesn't interfere with the layers
+        group.removeChildren().forEach((item) => {
+          paper.project.activeLayer.insertChild(index, item);
+        });
+      }
+    }
+  });
 }
