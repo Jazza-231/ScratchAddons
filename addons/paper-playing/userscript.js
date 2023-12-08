8610218b2d1e7ffd548dbe731efd576f04e29ec3
Jazza <greeny-231@outlook.com>
2023-12-08 22:00:34 +0800

    idk clean

--- addons/paper-playing/userscript.js
+++ addons/paper-playing/userscript.js
@@ -1,20 +1,52 @@
-export default async function ({ addon, console }) {
-  /*
+/*
 TODO (maybe):
-
+[Center on hotkey]
 Smooth button
-- Affects selected paths
 Rounded corners on rectangles
 x, y, width, height of costume items
 Polygons
 Costume editor arrow key modifiers
-Blending modes for shapes
+Blending modes for shapes?
 Look into shiftable anchor point
 
+NOTES:
+Smooth button
+1. Find out what the strings after the _ mean and how to make them
+2. Where will these new things go? All the "tools" require mouse input
+   And none of these require mouse input, there are more akin to the
+   outline width, etc
+3. Make sure to check the item type of the selected items, only smooth paths, not text etc
+4. Setting for smoothing amount?
+
+Rounded corners on rectangles
+1. UI from inkscape I think would be best
+2. Can you modify the rounding of the rectangle after its made?
+   With rectangle.radius or something? If not,
+   use math to move the points around the corners to change rounding amount
+
+x, y, width, height
+1. Where should UI go?
+
+Polygons
+1. New tool, select the number of points like the brush size
+2. Click and drag to set the size of the polygon
 */
 
+export default async function ({ addon, console }) {
   const paper = await addon.tab.traps.getPaper();
   console.log(paper);
+
+  function selectionBounds() {
+    paper.tools[0].boundingBoxTool.setSelectionBounds();
+  }
+
+  function updateImage() {
+    paper.tool.onUpdateImage();
+  }
+
+  function selectedItems() {
+    return paper.project.selectedItems;
+  }
 
   async function centerOnKey() {
     document.addEventListener("keydown", (e) => {
@@ -23,23 +55,25 @@ Look into shiftable anchor point
           // Prevent browser search opening
           e.preventDefault();
 
-          const selected = paper.project.selectedItems;
-          let index = 0;
-          selected.forEach((item) => {
-            // Find the highest item selected
-            if (item.index > index) index = item.index;
-          });
+          const selected = selectedItems();
+          if (selected) {
+            let index = 0;
+            selected.forEach((item) => {
+              // Find the highest index of all the selected items
+              if (item.index > index) index = item.index;
+            });
 
-          let group = new paper.Group(selected);
+            let group = new paper.Group(selected);
 
-          group.position = new paper.Point(480, 360);
-          paper.tools[0].boundingBoxTool.setSelectionBounds(); // Draw the bounding box in the new position
+            group.position = new paper.Point(480, 360);
+            selectionBounds();
 
-          group.remove(); // Remove the group so that it doesn't interfere with the layers
-          group.removeChildren().forEach((item) => {
-            paper.project.activeLayer.insertChild(index, item);
-          });
-          paper.tool.onUpdateImage();
+            group.remove(); // Remove the group so that it doesn't interfere with the layers
+            group.removeChildren().forEach((item) => {
+              paper.project.activeLayer.insertChild(index, item);
+            });
+            updateImage();
+          }
         }
       }
     });
