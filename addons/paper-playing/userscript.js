bd56205e32a5d427d6c39e1477668fd45007c330
Jazza <greeny-231@outlook.com>
2023-12-08 17:44:49 +0800

    update image

--- addons/paper-playing/userscript.js
+++ addons/paper-playing/userscript.js
@@ -1,30 +1,35 @@
 export default async function ({ addon, console }) {
   const paper = await addon.tab.traps.getPaper();
-  console.log(await addon.tab);
+  console.log(paper);
 
-  document.addEventListener("keydown", (e) => {
-    if (e.ctrlKey || e.metaKey) {
-      if (e.key === "e") {
-        // Prevent browser search opening
-        e.preventDefault();
+  async function centerOnKey() {
+    document.addEventListener("keydown", (e) => {
+      if (e.ctrlKey || e.metaKey) {
+        if (e.key === "e") {
+          // Prevent browser search opening
+          e.preventDefault();
 
-        const selected = paper.project.selectedItems;
-        let index = 0;
-        selected.forEach((item) => {
-          // Find the highest item selected
-          if (item.index > index) index = item.index;
-        });
+          const selected = paper.project.selectedItems;
+          let index = 0;
+          selected.forEach((item) => {
+            // Find the highest item selected
+            if (item.index > index) index = item.index;
+          });
 
-        let group = new paper.Group(selected);
+          let group = new paper.Group(selected);
 
-        group.position = new paper.Point(480, 360);
-        paper.tools[0].boundingBoxTool.setSelectionBounds(); // Draw the bounding box in the new position
+          group.position = new paper.Point(480, 360);
+          paper.tools[0].boundingBoxTool.setSelectionBounds(); // Draw the bounding box in the new position
 
-        group.remove(); // Remove the group so that it doesn't interfere with the layers
-        group.removeChildren().forEach((item) => {
-          paper.project.activeLayer.insertChild(index, item);
-        });
+          group.remove(); // Remove the group so that it doesn't interfere with the layers
+          group.removeChildren().forEach((item) => {
+            paper.project.activeLayer.insertChild(index, item);
+          });
+          paper.tool.onUpdateImage();
+        }
       }
-    }
-  });
+    });
+  }
+
+  centerOnKey();
 }
